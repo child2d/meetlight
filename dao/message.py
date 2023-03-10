abe3140394445823dfd078ee3b79391adfdcc5f9
perrychen <perry.chen@shaoke.com>
2023-03-10 11:52:58 +0800

    改了啥东西？

--- dao/message.py
+++ dao/message.py
@@ -1,22 +1,21 @@
 import datetime
 import json
-from typing import Dict, List
+import uuid
+from typing import Dict
 
 
-def get_all_message() -> List[Dict]:
+def get_all_message() -> Dict:
     with open("chat_history.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 
 def add_message(content, color):
-    messages.append(
-        {
-            "content": content,
-            "color": color,
-            "created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-        }
-    )
+    messages[uuid.uuid4().hex] = {
+        "content": content,
+        "color": color,
+        "created_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+    }
     with open("chat_history.json", "w", encoding="utf-8") as f:
         json.dump(messages, f)
 
